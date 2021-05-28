f189f20aedbfbe26729b544785ac17fd4fec82f0
wliu <wliu0709@gmail.com>
2021-05-28 14:10:29 -0400

    user.rb

--- cats_99_auth/app/models/user.rb
+++ cats_99_auth/app/models/user.rb
@@ -1,4 +1,41 @@
 class User < ApplicationRecord
   validates :user_name, :session_token, presence: true, uniqueness: true
   validates :password_digest, presence: true
-end
+  validates :password, length: {minimum: 6, allow_nill: true}
+  after_initialize :ensure_session_token
+
+def self.find_by_credentials(user_name, password)
+   user = User.find_by(user_name: user_name)
+   if user && user.is_password?(password)
+    return user
+   else
+    nil 
+   end 
+
+end 
+
+def ensure_session_token
+  self.session_token ||= SecureRandom::urlsafe_base64
+end 
+
+def password
+  @password
+end 
+
+def password=(password)
+  self.password_digest = BCrypt::Password.create(password)
+  @password = password
+end 
+
+def is_password?(password)
+  password_object = BCrypt::Password.new(self.password_digest)
+  password_object.is_password?(password)
+end 
+
+def reset_session_token!
+  self.session_token = SecureRandom::urlsafe_base64
+  self.save!
+  self.session_token
+end 
+
+end 
