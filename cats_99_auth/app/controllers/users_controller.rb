86be15ab03ee4bced21a6b3abe2dbda15a6834d3
wliu <wliu0709@gmail.com>
2021-05-28 15:33:22 -0400

    before using the session

--- cats_99_auth/app/controllers/users_controller.rb
+++ cats_99_auth/app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationRecord
+
+def new
+    render :new
+end 
+
+def create
+    @user = User.new(user_params)
+    if user.save
+        redirect_to user_url(@user)
+    else
+        render :new 
+    end 
+end 
+
+
+def user_params
+    params.require(:user).permit(:user_name, :password) 
+end
 end
